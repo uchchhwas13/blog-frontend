e3aeaaf54896973e47b0bf4dc7df2bd5aeb6e1f5
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-26 19:57:43 +0600

    Adding Model for Blog details

--- src/type/types.ts
+++ src/type/types.ts
@@ -17,3 +17,32 @@ type BlogListData = {
 };
 
 export type BlogListResponse = ApiResponse<BlogListData>;
+
+export type Comment = {
+  id: string;
+  content: string;
+  createdBy: {
+    name: string;
+    imageUrl: string;
+  };
+  createdAt: string;
+};
+
+export type BlogContent = {
+  id: string;
+  title: string;
+  body: string;
+  coverImageUrl: string;
+  createdAt: string;
+  createdBy: {
+    name: string;
+    imageUrl: string;
+  };
+};
+
+export type BlogDetailsModel = {
+  blog: BlogContent;
+  comments: [Comment];
+};
+
+export type BlogDetailsResponse = ApiResponse<BlogDetailsModel>;
