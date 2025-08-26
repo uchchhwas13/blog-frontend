7bed17929d85afef79a08fbbc38007d329ef15d4
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-26 20:06:59 +0600

    Adding fetch method for blog details

--- src/services/blogService.ts
+++ src/services/blogService.ts
@@ -1,11 +1,28 @@
 import axios from 'axios';
-import type { BlogListResponse } from '../type/types';
+import type { BlogDetailsResponse, BlogListResponse } from '../type/types';
 
 const API_BASE = 'http://localhost:3000';
 
 export const fetchBlogList = async (): Promise<BlogListResponse> => {
   try {
     const response = await axios.get<BlogListResponse>(`${API_BASE}/blogs`);
+    return response.data;
+  } catch (error) {
+    return {
+      success: false,
+      message: extractErrorMessage(error),
+      error: extractErrorMessage(error),
+    };
+  }
+};
+
+export const fetchBlogDetails = async (
+  id: string
+): Promise<BlogDetailsResponse> => {
+  try {
+    const response = await axios.get<BlogDetailsResponse>(
+      `${API_BASE}/blogs/${id}`
+    );
     return response.data;
   } catch (error) {
     return {
