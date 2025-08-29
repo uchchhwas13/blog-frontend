a26d05756e0743040f644343d97b6e6da9e130db
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-29 18:11:47 +0600

    Creating separate method to fetch refresh token

---
+++ src/services/refreshApi.ts
@@ -0,0 +1,31 @@
+// src/api/authApi.ts
+import axios from 'axios';
+import type {
+  RefreshTokenSuccessResponse,
+  RefreshTokenResponse,
+} from '../type/auth.types';
+
+const refreshAxios = axios.create({
+  baseURL: 'http://localhost:3000',
+});
+
+export const refreshAccessToken = async (
+  refreshToken: string
+): Promise<RefreshTokenSuccessResponse> => {
+  console.log('RefreshToken: ', refreshToken);
+  const response = await refreshAxios.post<RefreshTokenResponse>(
+    '/user/refresh-access-token',
+    {
+      refreshToken,
+    }
+  );
+  if (response.data.data) {
+    const data = response.data.data;
+    sessionStorage.setItem('refreshToken', data.refreshToken);
+    return {
+      accessToken: data.accessToken,
+      refreshToken: data.refreshToken,
+    };
+  }
+  throw Error('Invalid refresh token');
+};
