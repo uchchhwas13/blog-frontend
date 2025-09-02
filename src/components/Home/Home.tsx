5fa18ce43a968b2549ad25a96d58080aad4190e4
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-09-02 15:20:34 +0600

    Adding Shimmer effect for homepage

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from 'react';
 import type { BlogModel } from '../../type/blog.types';
 import { fetchBlogList } from '../../services/blogService';
 import { BlogItem } from './BlogItem';
+import HomePageShimmer from './HomePageShimmer';
 
 export const Home = (): React.JSX.Element => {
   const [blogs, setBlogs] = useState<BlogModel[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetchBlogList().then((response) => {
+      setIsLoading(false);
       if (response.error) {
         console.error(response.error); // show error in UI later
       } else {
@@ -16,6 +19,9 @@ export const Home = (): React.JSX.Element => {
     });
   }, []);
 
+  if (isLoading) {
+    return <HomePageShimmer />;
+  }
   return (
     <div className="p-10">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20">
